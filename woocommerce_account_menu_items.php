0aa1251ca0906ea317314c3ff13b9678a9002b87
SA Bappy <31420819+Bappyllcg@users.noreply.github.com>
2021-01-18 03:46:43 +0600

    My account page navigation

---
+++ woocommerce_account_menu_items.php
@@ -0,0 +1,13 @@
+// My account navigation 
+add_filter( 'woocommerce_account_menu_items', function($items) {
+    //unset( $items['dashboard'] ); // Remove Dashboard
+    //unset( $items['payment-methods'] ); // Remove Payment Methods
+    //unset( $items['orders'] ); // Remove Orders
+    unset( $items['downloads'] ); // Disable Downloads
+    unset( $items['edit-account'] ); // Remove Account details tab
+    unset( $items['customer-logout'] ); // Remove Logout link
+    $items['orders'] = 'My Orders'; 
+    $items['dashboard'] = 'My Information'; 
+
+    return $items;
+}, 99, 1 );
